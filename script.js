17e12f957f3e2f17a46919c5921aea4cb6f9e466
vivekkalal09 <vivekkalal09@gmail.com>
2025-08-19 16:05:26 +0530

    updated script.js and index.html

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/inr/usd.json";
+
+
 const dropdown = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdown) {
-    let newOption = document.createElement("option");
+    for (currCode in countryList) {
+        // console.log(`${countryList[currCode]}`);
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+        if (select.name === "from" && currCode == "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode == "INR") {
+            newOption.selected = "selected";
+        }
+
+    }
 }
+
+
 
